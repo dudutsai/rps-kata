f3b4f814268af94364c63294d6f26e17aaece86c
Andrew Tsai <andrewtsai@Andrew-Tsai-MacBook-Pro.local>
2019-09-10 10:08:56 -0400

    first test

--- src/backend/__tests__/playRPS.test.ts
+++ src/backend/__tests__/playRPS.test.ts
@@ -1,7 +1,7 @@
 import { play } from "../logic/playRPS";
 
 describe("Test the play logic", () => {
-  it("The play function returns `Hello World`", () => {
-    expect(play()).toEqual("Hello World!");
+  it("play returns 'p2 wins' in Scissors vs. Rock", () => {
+    expect(play("Scissors", "Rock")).toEqual("p2 wins!");
   });
 });
--- src/backend/logic/playRPS.ts
+++ src/backend/logic/playRPS.ts
@@ -1,3 +1,3 @@
 export function play() {
-  // Fix me!
+  return "p2 wins!";
 }
